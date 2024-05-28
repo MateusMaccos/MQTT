4c7888a55d2263cb2269961a2418244168be8e66
Mateus Machado Costa <mateusdoug@hotmail.com>
2024-05-28 08:34:44 -0300

    limpeza no código

--- application/configs/broker_configs.py
+++
@@ -1,7 +0,0 @@
-mqtt_broker_configs = {
-    "HOST": "test.mosquitto.org",
-    "PORT": 1883,
-    "CLIENT_NAME": "client_project",
-    "KEEPALIVE": 3,
-    "TOPICS": ["ppd/2024.1", "ppd/2024.2", "ppd/2024.3"],
-}
--- run.py
+++
@@ -1,4 +0,0 @@
-from application.main.main import start
-
-if __name__ == "__main__":
-    start()
